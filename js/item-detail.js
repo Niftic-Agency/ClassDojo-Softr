b1bda175af507506cc442fbf292a399cb3a089d7
Nathan Kennedy <nathan@niftic.agency>
2022-04-13 09:45:09 -0400

    add sectionCheck interval

--- js/item-detail.js
+++ js/item-detail.js
@@ -1,23 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
   // if all three sections exist then we're on a details page
   const sectionIds = ['list-details2','faq1','typeform-script-insert-dinamically']
-  if (sectionIds.every(id => document.getElementById(id))) {
 
-    // change the column widths of each section
-    sectionIds.forEach(sectionId => {
+  // wait until all three show up
+  const sectionCheck = setInterval(() => {
+    if (sectionIds.every(id => document.getElementById(id))) {
+      clearInterval(sectionCheck)
+  
+      // change the column widths of each section
+      sectionIds.forEach(sectionId => {
+  
+        // wrap in setInterval to make sure column classes don't get overwritten
+        const colCheck = setInterval(() => {
+          const section = document.getElementById(sectionId);
+          const container = section.firstElementChild;
+          const rows = Array.from(container.children);
+          rows.forEach(row => {
+            if (row.firstElementChild.className !== `col-12 col-lg-8 col-${sectionId}`) row.firstElementChild.className = `col-12 col-lg-8 col-${sectionId}`;
+          }) 
+        }, 100)
+  
+        // clean up interval after 10 seconds
+        setTimeout(() => clearInterval(colCheck), 10000)
+      })
+    }
+  })
 
-      // wrap in setInterval to make sure column classes don't get overwritten
-      const colCheck = setInterval(() => {
-        const section = document.getElementById(sectionId);
-        const container = section.firstElementChild;
-        const rows = Array.from(container.children);
-        rows.forEach(row => {
-          if (row.firstElementChild.className !== `col-12 col-lg-8 col-${sectionId}`) row.firstElementChild.className = `col-12 col-lg-8 col-${sectionId}`;
-        }) 
-      }, 100)
-
-      // clean up interval after 10 seconds
-      setTimeout(() => clearInterval(colCheck), 10000)
-    })
-  }
+  // clean up interval after 10 seconds if section don't exist on page
+  setTimeout(() => clearInterval(sectionCheck), 10000)
 });
