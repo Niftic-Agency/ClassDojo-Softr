012a8b96f3e8c40c219fe59060ab5c3d12bc75a8
Nathan Kennedy <nathan@niftic.agency>
2022-04-13 12:35:15 -0400

    disable item detail js

--- js/item-detail.js
+++ js/item-detail.js
@@ -1,65 +1,67 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // if all three sections exist then we're on a details page
-  const sectionIds = ['list-details2','faq1','typeform-script-insert-dinamically']
+// document.addEventListener("DOMContentLoaded", function() {
+//   // if all three sections exist then we're on a details page
+//   const sectionIds = ['list-details2','faq1','typeform-script-insert-dinamically']
 
-  // wait until all three show up
-  const sectionCheck = setInterval(() => {
-    if (sectionIds.every(id => document.getElementById(id))) {
-      clearInterval(sectionCheck)
+//   // wait until all three show up
+//   const sectionCheck = setInterval(() => {
+//     if (sectionIds.every(id => document.getElementById(id))) {
+//       clearInterval(sectionCheck)
   
-      // change the column widths of each section
-      sectionIds.forEach(sectionId => {
+//       // change the column widths of each section
+//       sectionIds.forEach(sectionId => {
   
-        // wrap in setInterval to make sure column classes don't get overwritten
-        const colCheck = setInterval(() => {
-          const section = document.getElementById(sectionId);
-          const container = section.firstElementChild;
-          const rows = Array.from(container.children);
-          rows.forEach(row => {
-            if (row.firstElementChild.className !== `col-12 col-lg-8 col-${sectionId}`) row.firstElementChild.className = `col-12 col-lg-8 col-${sectionId}`;
-          }) 
-        }, 100)
+//         // wrap in setInterval to make sure column classes don't get overwritten
+//         const colCheck = setInterval(() => {
+//           const section = document.getElementById(sectionId);
+//           const container = section.firstElementChild;
+//           const rows = Array.from(container.children);
+//           rows.forEach(row => {
+//             if (row.firstElementChild.className !== `col-12 col-lg-8 col-${sectionId}`) row.firstElementChild.className = `col-12 col-lg-8 col-${sectionId}`;
+//           }) 
+//         }, 100)
   
-        // clean up interval after 10 seconds
-        setTimeout(() => clearInterval(colCheck), 10000)
-      })
+//         // clean up interval after 10 seconds
+//         setTimeout(() => clearInterval(colCheck), 10000)
+//       })
 
 
-      buildSidebar();
-    }
-  })
+//       buildSidebar();
+//     }
+//   })
 
-  // clean up interval after 10 seconds if section don't exist on page
-  setTimeout(() => clearInterval(sectionCheck), 10000)
-});
+//   // clean up interval after 10 seconds if section don't exist on page
+//   setTimeout(() => clearInterval(sectionCheck), 10000)
+// });
 
 
-function buildSidebar() {
-  const sidebarDataCheck = setInterval(() => {
-    if ("itemDetailsSidebar" in window) {
-      clearInterval(sidebarDataCheck);
+// function buildSidebar() {
+//   const sidebarDataCheck = setInterval(() => {
+//     if ("itemDetailsSidebar" in window) {
+//       clearInterval(sidebarDataCheck);
 
-      const d = window.itemDetailSidebar;
+//       const d = window.itemDetailSidebar;
 
-      const sidebar = document.createElement('div');
-      sidebar.className = "item-details-sidebar";
-      sidebar.innerHTML = `
-        <div class="sidebar-wrap">
-          <div class="sidebar-box">
-            <h3>${d.course}</h3>
-            <p>${d.fdate}</p>
-            <div class="teacher">
-              <img src="${d.teacherpicture[0].url}" alt="${d.teacher} image">
-              <div class="label">${d.teacher}</div>
-            </div>
-            <button>Enroll in class</button>
-          </div>
-        </div>
-      `
+//       const sidebar = document.createElement('div');
+//       sidebar.className = "item-details-sidebar";
+//       sidebar.innerHTML = `
+//         <div class="sidebar-wrap">
+//           <div class="sidebar-box">
+//             <h3>${d.course}</h3>
+//             <p>${d.fdate}</p>
+//             <div class="teacher">
+//               <img src="${d.teacherpicture[0].url}" alt="${d.teacher} image">
+//               <div class="label">${d.teacher}</div>
+//             </div>
+//             <button>Enroll in class</button>
+//           </div>
+//         </div>
+//       `
 
-      const row = document.querySelector('list-details2 > .container > .row')
-      row.insertBefore(sidebar, row.firstElementChild);
+//       const row = document.querySelector('list-details2 > .container > .row')
+//       row.insertBefore(sidebar, row.firstElementChild);
       
-    }
-  }, 100)
-}
+//     }
+//   }, 100)
+// }
+
+
